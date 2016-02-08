61be131e087b828aeabf2866a078e54eecaf340f
Dimitri Rosenberg <dimitri@range-app.com>
2016-02-08 18:12:28 +0300

    feat(socket): implement connection to rethink via sockets

--- app/socket/socket.ts
+++ app/socket/socket.ts
@@ -1,11 +1,21 @@
-'use strict';
-
+import * as r from 'rethinkdb';
 import * as ws from 'ws';
 
+import { Subject } from 'rxjs/Subject';
+import { ReplaySubject } from 'rxjs/subject/ReplaySubject';
+import { Subscriber } from 'rxjs/Subscriber';
+
+import { verifyClient } from './utils/verify_client';
+
 export const app = (httpServer) => {
-  const wsServer = new ws.Server({ server: httpServer, path: '/echo' });
+  const wsServer = new ws.Server({ server: httpServer, path: '/echo', verifyClient });
 
   wsServer.on('connection', ws => {
-    console.log('Socket connection established');
+    let { db } = <any>ws.upgradeReq;
+
+    ws.on('close', () => {
+      db.close();
+    });
   });
 }
+
---
+++ app/socket/utils/verify_client.ts
@@ -0,0 +1,19 @@
+import * as qs from 'qs'
+import * as r from 'rethinkdb';
+import * as url from 'url';
+
+export const verifyClient = async ({ origin, req, secure }, done) => {
+  try {
+    let { db } = qs.parse(url.parse(req.url).query);
+
+    req.db = await r.connect({ host: process.env['RETHINK_HOST'], port: process.env['RETHINK_PORT'] })
+
+    if (!(await r.dbList().contains(db).run(req.db))) {
+      throw `Database "${db}" doesn't exists`;
+    }
+
+    done(true)
+  } catch (err) {
+    done(false, 400, err.toString())
+  }
+}
