ca7b0ebe32f75952a01395a4bc397fba8571a1aa
Dimitri Rosenberg <dimitri@range-app.com>
2016-03-08 18:51:41 +0300

    feat(clean): remove unused imports

--- app/server/database.ts
+++ app/server/database.ts
@@ -4,11 +4,3 @@ import * as knex from 'knex';
 const config = require('../../../../db/config');
 
 Model.knex(knex(config[process.env.NODE_ENV || 'development']));
-
-// remove
-
-import * as bookshelf from 'bookshelf';
-export const Database = bookshelf(knex(config[process.env.NODE_ENV || 'development']));
-
-
-Database.plugin('virtuals');
