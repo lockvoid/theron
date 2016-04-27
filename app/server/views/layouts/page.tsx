ff943f30d9aba00d33de0577d64b49b47536b48f
Dimitri Rosenberg <dimitri@range-app.com>
2016-04-27 20:13:29 +0300

    feat(seo): update the description and keyword meta tags

--- app/server/views/layouts/page.tsx
+++ app/server/views/layouts/page.tsx
@@ -10,7 +10,8 @@ export const Page = ({ title = null, children = null, className = null }) => (
 
       <base href="/" />
 
-      <meta name="description" content="Reactive storage for realtime apps." />
+      <meta name="description" content="Reactive storage for realtime applications. Theron instantly streams data across clients, so you can focus on creating an extraordinary experience for your users." />
+      <meta name="keywords" content="realtime, reactive, websockets, angular, react, ember, rxjs, browser, mobile, instant updates, push notifications, automatic scaling, free sign up" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
       <link rel="stylesheet" media="screen" href={assetPath('app.css')} />
