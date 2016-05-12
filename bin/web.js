b5355021b61f041e4c62af4619a8056a40c752c2
Dimitri Rosenberg <dimitri@range-app.com>
2016-05-13 00:38:22 +0300

    feat(bin): enable concurrency

--- bin/web.js
+++ bin/web.js
@@ -1,21 +1,29 @@
 'use strict';
 
 const http = require('http');
+const os = require('os');
+const throng = require('throng');
 
-// Import internal modules
+function start(id) {
+  // Import
 
-const server = require('../dist/server/app/server/server');
-const socket = require('../dist/server/app/server/socket');
+  require('../dist/server/app/server/database');
 
-require('../dist/server/app/server/database');
+  const server = require('../dist/server/app/server/server');
+  const socket = require('../dist/server/app/server/socket');
 
-// Bootstrap internal services
+  // Bootstrap
 
-const httpServer = http.createServer(server.app)
-const serverPort = process.env.PORT || 9090;
+  const httpServer = http.createServer(server.app)
+  const PORT = process.env.PORT || 9090;
 
-socket.app(httpServer);
+  socket.app(httpServer);
 
-httpServer.listen(serverPort, () => {
-  console.log(`Server listening on: http://0.0.0.0:${serverPort}`);
-});
+  httpServer.listen(PORT, () => {
+    console.log(`Server ${id} is listening on: http://0.0.0.0:${PORT}`);
+  });
+}
+
+const WORKERS = process.env.NODE_ENV === 'production' ? process.env.WEB_CONCURRENCY : os.cpus().length;
+
+throng({ start, workers: WORKERS, lifetime: Infinity });
