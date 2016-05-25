aec5e1a4e83129ceaf3ccaf3d98dccacb626e3ba
Dimitri Rosenberg <dimitri@range-app.com>
2016-05-25 14:21:00 +0300

    refactor(utils): don't fail with raygun fail

--- app/server/utils/log_error.ts
+++ app/server/utils/log_error.ts
@@ -5,5 +5,10 @@ if (process.env.RAYGUN_KEY) {
 }
 
 export function logError(err) {
-  raygun ? raygun.send(err, {}) : console.error(err);
+  try {
+    raygun ? raygun.send(err, {}) : console.error(err);
+  } catch (raygun) {
+    console.error(raygun);
+    console.error(err);
+  }
 }
